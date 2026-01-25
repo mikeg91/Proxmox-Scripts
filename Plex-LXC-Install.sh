637bea9cfa17f4e2229f53a899b9b0bb38ca9e29
mikeg91 <michaelgrady91@gmail.com>
2026-01-25 14:32:32 -0500

    Modify sources.list for Debian Bookworm
    Updated Debian sources.list for Bookworm release.

--- Plex-LXC-Install.sh
+++ Plex-LXC-Install.sh
@@ -10,9 +10,12 @@ NC='\033[0m'
 echo -e "${GREEN}Configuring Debian Bookworm sources...${NC}"
 
 cat <<'EOF' > /etc/apt/sources.list
-deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
-deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
-deb http://security.debian.org bookworm-security main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
 EOF
 
 echo -e "${GREEN}Updating system...${NC}"
